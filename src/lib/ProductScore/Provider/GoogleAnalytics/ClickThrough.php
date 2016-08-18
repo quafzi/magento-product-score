362ad3c26325d2eecde823e16ca171cd175d95b2
Thomas Birke <quafzi@netextreme.de>
2016-08-18 13:39:56 +0200

    Add provider GoogleAnalytics clickThrough

---
+++ src/lib/ProductScore/Provider/GoogleAnalytics/ClickThrough.php
@@ -0,0 +1,32 @@
+<?php
+namespace Quafzi\ProductScore\Provider\GoogleAnalytics;
+
+use Quafzi\ProductScore\Item\Consumer\ConsumerInterface as Consumer;
+use Quafzi\ProductScore\Provider\ProviderInterface as Provider;
+use Quafzi\ProductScore\Provider\FetchException;
+use Quafzi\ProductScore\Provider\GoogleAnalytics\GoogleAnalyticsAbstract as GoogleAnalytics;
+
+/**
+ * Google Analytics Click-Through Product Score Provider
+ *
+ * @author Thomas Birke <magento@netextreme.de>
+ */
+
+class ClickThrough extends GoogleAnalytics
+{
+    /**
+     * fetch score information
+     *
+     * @param Consumer $consumer Handler for fetched item score
+     * @param array    $config   Provider configuration data
+     *
+     * @return $this
+     */
+    public function fetch(Consumer $consumer, array $config)
+    {
+        $this->consumer = $consumer;
+        $this->fetchAnalyticsColumn('ga:productListCTR', $config);
+
+        return $this;
+    }
+}
